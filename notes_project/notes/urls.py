67ac16767ef679a140fed9dc2d91132372118469
yonatan semate <semateyonatan@gmail.com>
2025-06-14 23:10:56 +0300

    notes app startup

---
+++ notes_project/notes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('add/', views.add_note, name='add_note'),
+    path('edit/<int:note_id>/', views.edit_note, name='edit_note'),
+    path('delete/<int:note_id>/', views.delete_note, name='delete_note'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='signup'),
+
+]
